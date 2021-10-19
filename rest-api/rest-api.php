a1a59ad7d14f77ac89f51d9d48501d9767e88fad
Prykon <dariomanoukian@gmail.com>
2021-10-19 13:57:08 -0300

    Added leader ratio by gender and get_text auxiliary function

--- rest-api/rest-api.php
+++ rest-api/rest-api.php
@@ -12,9 +12,6 @@ class Disciple_Tools_Advanced_Metrics
 
 
     /**
-     * @todo define the name of the $namespace
-     * @todo define the name of the rest route
-     * @todo defne method (CREATABLE, READABLE)
      * @todo apply permission strategy. '__return_true' essentially skips the permission check.
      */
     //See https://github.com/DiscipleTools/disciple-tools-theme/wiki/Site-to-Site-Link for outside of wordpress authentication
@@ -37,28 +34,17 @@ class Disciple_Tools_Advanced_Metrics
                 'callback' => [ $this, 'get_bible_reading_ratio' ],
             ]
         );
+
+        register_rest_route(
+            $namespace, '/get_leader_gender_ratio', [
+                'methods' => 'GET',
+                'callback' => [ $this, 'get_leader_gender_ratio' ],
+            ]
+        );
     }
 
-    // public function get_data( WP_REST_Request $request ) {
-    //     global $wpdb;
-    //     $output = [];
-    //     $response = $wpdb->get_col( $wpdb->prepare( "
-    //         SELECT count(post_type) AS contact_count
-    //         FROM $wpdb->posts
-    //         WHERE post_type = 'contacts'" ) );
-    //     $output['contact_count'] = [ intval($response[0]) ];
 
-    //     $response = $wpdb->get_results( $wpdb->prepare( "
-    //         SELECT REPLACE( meta_value, '-', '') as date, COUNT( * ) as baptism_count
-    //         FROM wp_dt_activity_log
-    //         WHERE meta_key = 'baptism_date'
-    //         AND object_note LIKE 'Added Baptism Date: %'
-    //         GROUP BY meta_value;" ) );
-    //         $output['baptism_count'] = $response;
-
-    //     return $output;
-    // }
-
+    // Get the amount of men compared to women
     public function get_gender_ratio( WP_REST_Request $request ) {
         $output = null;
         $contact_ids = self::get_contact_ids();
@@ -83,9 +69,12 @@ class Disciple_Tools_Advanced_Metrics
             $output['male_count'] = $male_count;
             $output['female_count'] = $female_count;
 
+            $output['male_ratio'] = 0;
+            $output['female_ratio'] = 0;
+
             // Get Male/Female Ratio
             $output['ratio'] = 0;
-            if ( $male_count != 0 ) {
+            if ( $male_count > 0 ) {
                 $output['ratio'] = $male_count / $female_count;
             }
 
@@ -93,25 +82,96 @@ class Disciple_Tools_Advanced_Metrics
             $male_ratio = $male_count / $gcd;
             $female_ratio = $female_count / $gcd;
 
-            $male_text = 'man';
-            $female_text = 'woman';
-            $is_text = 'is';
+            $output['male_ratio'] = $male_ratio;
+            $output['female_ratio'] = $female_ratio;
 
-            if ( $male_ratio > 1 ) {
-                $male_text = 'men';
-                $is_text = 'are';
-            }
-
-            if ( $female_ratio > 1 ) {
-                $female_text = 'women';
-                $is_text = 'are';
-            }
+            $male_text = self::get_text( 'man', 'men', $male_ratio );
+            $female_text = self::get_text( 'woman', 'women', $female_ratio );
+            $is_text = self::get_text( 'is', 'are', $female_ratio );
 
             $output['description'] = "There $is_text $male_ratio $male_text for every $female_ratio $female_text.";
             return $output;
         }
     }
 
+
+    // Get the amount of male leaders compared to female leaders
+    public function get_leader_gender_ratio( WP_REST_Request $request ) {
+        $output = null;
+        global $wpdb;
+
+        $response = $wpdb->get_results("
+            SELECT post_id
+            FROM wp_postmeta
+            WHERE (meta_key = 'gender' AND meta_value = 'male')
+            OR (meta_key = 'faith_status' AND meta_value = 'leader')
+            GROUP BY post_id
+            HAVING COUNT(post_id) > 1;
+            ");
+
+        $male_leader_count = $wpdb->num_rows;
+
+
+        $response = $wpdb->get_results("
+            SELECT post_id
+            FROM wp_postmeta
+            WHERE (meta_key = 'gender' AND meta_value = 'female')
+            OR (meta_key = 'faith_status' AND meta_value = 'leader')
+            GROUP BY post_id
+            HAVING COUNT(post_id) > 1;
+            ");
+
+        $female_leader_count = $wpdb->num_rows;
+
+        $output['male_leader_count'] = $male_leader_count;
+        $output['female_leader_count'] = $female_leader_count;
+
+        $output['gender_leader_ratio'] = 0;
+        if ( $male_leader_count > 0 ) {
+            $output['gender_leader_ratio'] = $female_leader_count / $male_leader_count;
+        }
+
+        $output['male_leader_ratio'] = 0;
+        $output['female_leader_ratio'] = 0;
+
+
+        if ( $female_leader_count === 0 ) {
+            $leader_text = self::get_text( 'leader', 'leaders', $male_leader_count );
+            $is_text = self::get_text( 'is', 'are', $male_leader_count );
+            $output['description'] = "There $is_text $male_leader_count male $leader_text and no female leaders.";
+        }
+
+        if ( $male_leader_count === 0 ) {
+            $leader_text = self::get_text( 'leader', 'leaders', $female_leader_count );
+            $is_text = self::get_text( 'is', 'are', $female_leader_count );
+            $output['description'] = "There $is_text $female_leader_count $leader_text and no male leaders.";
+        }
+
+        if ( $male_leader_count === 0 && $female_leader_count === 0 ) {
+            $output['description'] = 'There are no leaders. Yet.';
+            return $output;
+        }
+
+        if ( $male_leader_count > 0 && $female_leader_count > 0 ) {
+            $gcd = self::get_gcd( $female_leader_count, $male_leader_count );
+            $male_leader_ratio = $male_leader_count / $gcd;
+            $female_leader_ratio = $female_leader_count / $gcd;
+
+            $output['male_leader_ratio'] = $male_leader_ratio;
+            $output['female_leader_ratio'] = $female_leader_ratio;
+
+            $is_text = self::get_text( 'is', 'are', $female_leader_ratio );
+            $leader_male_text = self::get_text( 'leader', 'leaders', $male_leader_ratio );
+            $leader_female_text = self::get_text( 'leader', 'leaders', $female_leader_ratio );
+
+            $output['description'] = "There $is_text $female_leader_ratio female $leader_female_text for every $male_leader_ratio male $leader_male_text.";
+        }
+
+        return $output;
+    }
+
+
+    // Get the amount of Bible readers for every Bible owner
     public function get_bible_reading_ratio( WP_REST_Request $request ) {
         global $wpdb;
         $output = null;
@@ -134,28 +194,51 @@ class Disciple_Tools_Advanced_Metrics
         $output['reading_bible'] = $reading_bible_count;
         $output['ratio'] = 0;
 
-        //Don't divide by 0
-        if ( $has_bible_count != 0 ) {
+        $output['reading_bible_ratio'] = 0;
+        $output['has_bible_ratio'] = 0;
+
+        $output['description'] = 'No Bibles were distributed yet.';
+
+        // Don't divide by 0
+        if ( $has_bible_count > 0 ) {
             $output['ratio'] = $reading_bible_count / $has_bible_count;
         }
 
-        //Get Greatest Common Denominators
-        $bible_gcd = self::get_gcd( $reading_bible_count, $has_bible_count );
-
-        //Get Have/Read Ratio
-        $reading_bible_ratio = $reading_bible_count / $bible_gcd;
-        $has_bible_ratio = $has_bible_count / $bible_gcd;
-
-        $is_text = 'is';
-        if ( $reading_bible_ratio > 1 ) {
-            $is_text = 'are';
+        if ( $has_bible_count > 0 && $reading_bible_count === 0 ) {
+            $bible_was_text = self::get_text( 'Bible was', 'Bibles were', $has_bible_count );
+            $nobody_is_reading_it_text = self::get_text( 'nobody is reading it', 'nobody is reading them', $has_bible_count );
+            $output['description'] = "$has_bible_count $bible_was_text distributed but $nobody_is_reading_it_text (that we know of).";
         }
 
-        $output['description'] = "$reading_bible_ratio out of $has_bible_ratio contacts $is_text reading their Bible.";
+        // Get Have/Read Ratio
+        if ( $has_bible_count > 0 && $reading_bible_count > 0 ) {
+            // Get Greatest Common Denominators
+            $gcd = self::get_gcd( $reading_bible_count, $has_bible_count );
+
+            $reading_bible_ratio = $reading_bible_count / $gcd;
+            $has_bible_ratio = $has_bible_count / $gcd;
+
+            $output['has_bible_ratio'] = $has_bible_ratio;
+            $output['reading_bible_ratio'] = $reading_bible_ratio;
+
+            $is_text = self::get_text( 'is', 'are', $reading_bible_ratio );
+            $contact_is_text = self::get_text( 'contact is', 'contacts are', $has_bible_ratio );
+            $his_text = self::get_text( 'his', 'their', $has_bible_ratio );
+            $output['description'] = "$reading_bible_ratio out of every $has_bible_ratio $contact_is_text reading $his_text Bible.";
+        }
 
         return $output;
     }
 
+
+    // Check if the wording for a metric should be plural or singular, according to the metric count
+    private function get_text( $text_singular, $text_plural, $metric ) {
+        $output = $text_singular;
+        if ( $metric > 1 ) {
+            $output = $text_plural;
+        }
+        return $output;
+    }
 
 
     private function get_factors( $num ) {
